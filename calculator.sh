a92cf900a2f8925ee525a95e072d314b458dcbac
root <root@DESKTOP-FVO412M>
2023-07-05 11:30:36 -0400

    This file will calcuate the numbers

---
+++ calculator.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#####################################
+# Autor: Karthik
+# Date: 7/5/2023
+# Version: V1
+# This file will do the calculation
+#####################################
+y= a+b
